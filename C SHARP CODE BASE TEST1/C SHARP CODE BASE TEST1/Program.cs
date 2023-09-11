fb60586e7f9581a70a9d83c42e0c02ed33d17f4d
Varsha Nanjegowda <varshan@infinite.com>
2023-09-11 10:41:36 +0530

    test code

---
+++ C SHARP CODE BASE TEST1/C SHARP CODE BASE TEST1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODE_BASE_TEST1
+{
+    class Program
+    {
+        static string RemoveCharacterAtPosition(string input, int position)
+        {
+            if(position<0|| position>=input.Length)
+            {
+                return input;
+            }
+            return input.Substring(0, position) + input.Substring(position + 1);
+        }
+        static void Main(string[] args)
+        {
+            string input1 = "Coding";
+            int position1 = 1;
+            string result1 = RemoveCharacterAtPosition(input1, position1);
+            Console.WriteLine(result1);
+            string input2 = "Program";
+            int position2 = 4;
+            string result2 = RemoveCharacterAtPosition(input2, position2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C SHARP CODE BASED TEST1(b)/C SHARP CODE BASED TEST1(b)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_CODE_BASED_TEST1_b_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                for (int k = 0; k<= 10; k++)
+                {
+                    int result = num * k;
+                    Console.WriteLine($"{num}*{k}={result}");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("invalid input.please enter a valid number.");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
