ea76865be9971d30fb8ba54a94dd86146eb5222d
Varsha Nanjegowda <varshan@infinite.com>
2023-09-19 10:49:43 +0530

    test2

---
+++ CSHARP CODE BASE TEST  2B/CSHARP CODE BASE TEST  2B/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_CODE_BASE_TEST__2B
+{
+    class Products
+    {
+        int Productid;
+        string ProductName;
+        double Price;
+
+        public Products(int productid, string productname, double price)
+        {
+            Productid = productid;
+            ProductName = productname;
+            Price = price;
+        }
+    
+
+        static void Main(string[] args)
+        {
+        List<Products> products = new List<Products>();
+        for (int i=1;i<=10; i++)
+        {
+            Console.WriteLine("enter product details");
+            Console.WriteLine("enter product id");
+            int Productid = int.Parse(Console.ReadLine());
+            Console.Write("Product Name: ");
+            string ProductName = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = double.Parse(Console.ReadLine());
+             products.Add(new Products(Productid, ProductName, price));
+            products.Sort((n1, n2) => n1.Price.CompareTo(n2.Price));
+            Console.WriteLine("  Sorted Products by Price:");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.Productid}, Product Name: {product.ProductName}, Price: {product.Price}");
+            }
+        }
+        Console.WriteLine("");
+            Console.ReadLine();
+        }
+        }
+    }
+
---
+++ CSHARP CODE BASE TEST 2/CSHARP CODE BASE TEST 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHARP_CODE_BASE_TEST_2
+{
+
+    public abstract class Student
+    {
+        public string name;
+        public int StudentId;
+        public double Grade;
+        public abstract bool IsPassed(double grade);
+    }
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Undergraduate undergradStudent = new Undergraduate
+            {
+                name = "VARSHA",
+                StudentId = 103,
+                Grade = 60.0
+
+            };
+            Graduate gradstudent = new Graduate
+            {
+                name = "KEERTHANA",
+                StudentId = 121,
+                Grade = 85.5
+            };
+            Console.WriteLine($"{undergradStudent.name} passed:{undergradStudent.IsPassed(undergradStudent.Grade)}");
+            Console.WriteLine($"{gradstudent.name} passed:{gradstudent.IsPassed(gradstudent.Grade)}");
+            Console.ReadLine();
+        }
+    }
+}
+        
